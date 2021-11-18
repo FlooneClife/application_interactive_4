81586c657c7ee277f72a1dbcf70449fdaf01a251
COUNILLE Alexandra <alexandra.fmc@outlook.fr>
2021-11-18 12:07:31 +0100

    Update gameObject.h

--- gameObject.h
+++ gameObject.h
@@ -3,27 +3,42 @@
 #include <stdint.h>
 #include <vector>
 #include <string.h>
-
-//mesh à chaque gameobject
-//hitbox
-//drawable?
-
+#include <QMatrix4x4>
+#include "transform.h"
 
 class GameObject {
 
     char* name;
-    GameObject parent; //si c'est la scene, la référence est nulle
-    std::vector<GameObject> children;
+    GameObject* parent; //si c'est la scene, la référence est nulle
+    std::vector<GameObject*> children;
+    std::vector<QVector4D> vertices;
+    std::vector<QVector4D> texCoords;
+    bool drawable;
 
 public:
-    GameObject();
+    GameObject(char* name, GameObject* parent, bool drawable) {
+        this->name = name;
+        this->parent = parent;
+        this->drawable = drawable;
+    }
 
     void addChild(GameObject &child) {
-        children.push_back(child);
+        children.push_back(&child);
     }
 
     void assignParent(GameObject &parent) {
-        this->parent = parent;
+        this->parent = &parent;
+    }
+    
+    void transform(float scale, QVector3D angle, QVector3D translation) {
+        for(QVector4D p : vertices) {
+            Transform::trTransform(scale, angle, translation, p);
+        }
+        for(GameObject* child : children) {
+            for(QVector4D p : child->vertices) {
+                Transform::trTransform(scale, angle, translation, p);
+            }
+        }
     }
 
 private:
